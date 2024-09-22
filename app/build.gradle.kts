29df4f2533864d75ee9160415b3aa488ca429d5b
george <geomel@yandex.ru>
2024-09-22 19:33:55 +0300

    step1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation ("org.assertj:assertj-core:3.25.2")
+    testImplementation("org.assertj:assertj-core:3.25.2")
 }
 tasks.test {
     useJUnitPlatform()
