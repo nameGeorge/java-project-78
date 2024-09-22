daa7fee89be3efef136fa7613ed2497e4847d421
george <geomel@yandex.ru>
2024-09-22 21:29:32 +0300

    step1.3

--- Makefile
+++ Makefile
@@ -1,27 +1,24 @@
 .DEFAULT_GOAL := build-run
-
+setup:
+	make -C app setup
 clean:
-  make -C ./app clean
-
-lint:
-  make -C ./app lint
-
-run-dist:
-  make -C ./app run-dist
-
-test:
-  make -C ./app test
-
+	make -C app clean
 build:
-  make -C ./app build
-
-.PHONY: build
-
-run-json:
-  make -C ./app run-json
-
-run-yaml:
-  make -C ./app run-yaml
-
+	make -C app build
+install:
+	make -C app install
+run-dist:
+	make -C app run-dist
+run:
+	make -C app run
+test:
+	make -C app test
 report:
-	make -C ./app report
+	make -C app report
+lint:
+	make -C app lint
+check-deps:
+	make -C app check-deps
+build-run:
+	make -C app build-run
+.PHONY: build
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,35 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
-    id("java")
     application
-    id("checkstyle")
+    checkstyle
     jacoco
 }
-application { mainClass.set("hexlet.code.App") }
+application {
+    mainClass = "hexlet.code.App"
+}
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.assertj:assertj-core:3.25.2")
 }
 tasks.test {
     useJUnitPlatform()
-    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 tasks.jacocoTestReport {
     reports {
-        xml.required = true
-        csv.required = false
-        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+        xml.required.set(true)
     }
 }
