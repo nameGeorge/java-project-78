3dc1b72d5b34ab4e9713f8baa71aaace08c29c0f
george <geomel@yandex.ru>
2024-09-22 19:12:03 +0300

    step1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,29 @@
 plugins {
     id("java")
+    application
+    id("checkstyle")
+    jacoco
 }
-
+application { mainClass.set("hexlet.code.App") }
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation ("org.assertj:assertj-core:3.25.2")
 }
-
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
 }
