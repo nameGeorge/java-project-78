e05db67b4cd3517924c1cb3ae6f5bdc2433bb6d0
george <geomel@yandex.ru>
2024-09-29 10:42:40 +0300

    fix hexlet-check errors

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -11,12 +11,23 @@ public class BaseSchema<T> {
         requirements = new HashMap<>();
     }
 
+    /**
+     * Adds a new validation requirement.
+     *
+     * @param name        - requirement name
+     * @param requirement - validation requirement
+     */
     public void addRequirement(String name, Predicate<T> requirement) {
         requirements.put(name, requirement);
     }
 
+    /**
+     * Checks the value for the validity of the scheme.
+     *
+     * @param dataForValidation - data for validation
+     * @return - true if data is valid, else false
+     */
     public boolean isValid(T dataForValidation) {
-
         return requirements.values().stream()
                 .allMatch(requirement -> requirement.test(dataForValidation));
     }
